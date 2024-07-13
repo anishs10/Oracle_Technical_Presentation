c72c5dfd8763362efd01e0b3b465f516033c387c
Anish kumar <48911049+anishs10@users.noreply.github.com>
2024-07-13 22:31:46 +0530

    Add files via upload

---
+++ Oracle Certification Benefits.sql
@@ -0,0 +1,116 @@
+Oracle Certification Benefits
+
+-- https://education.oracle.com/certification-benefits
+
+Get Certified to Stand Out in a Crowd
+
+Reap the Benefits of Earning an Oracle Certification
+
+Expand your knowledge base and validate your skills to appeal to potential employers.
+Improve your potential earning power to command a higher salary.
+Learn to perform complex, hands-on activities through lab, study and practice sessions.
+Access a secure digital badge you can add to your social media profiles.
+Gain exposure to a wide variety of important features, functions and tasks to use on the job.
+
+=====
+
+Answer real-world, scenario-based questions.
+Test your skills & validate your capabilities by engaging in real-world scenarios.
+
+====
+Becoming Oracle Certified will make you a better technologist.
+
+Preparing for an Oracle Certification Exam will assess & challenge your ability to think & perform. Earning a highly-recognized & valued Oracle Certification will improve your job prospects & job stability, accelerating your career growth.
+View the certification level chart below to learn more.
+
+
+==
+Oracle Program Certification Levels         Associate    Professional	Master
+
+Receive a secure digital badge that you can share with your professional network across social media profile
+
+	YES	YES	YES
+
+Get a personalized eCertificate available for immediate download and printing.	      
+
+                          YES	YES	YES
+
+Receive a high quality apparel item to display your certification achievement. 
+
+                             	NO	NO	YES
+
+Take all actions related to your certifications in one convenient place 
+
+                                    	YES	YES	YES
+
+Easily verify your certification to your manager, a potential employer, or anyone else
+Higher average salary compared with non-certified colleagues	
+
+YES	YES	YES
+
+Preferred access to cloud based employment	YES	YES	YES
+
+
+Oracle Partner Network company membership requirements
+
+===
+The benefits of Oracle Certification
+
+Oracle Certification enables professionals to demonstrate in-demand skills, shorten project turnaround time, improve overall job performance, and increase their earning potential - all while boosting on-the-job confidence.
+
+Credibility
+Empowerment
+Recognition
+Satisfaction
+
+==
+Professional credibility
+Professional credibility comes from training + certification combined. Learning, retained longer through the process of certification preparation = knowledge and credibility.
+
+75%
+"able to perform a task or fill a role they were unable to before"
+81%
+"increased quality and value of work contributions"
+77%
+"ability to innovate and enhance work processes and outcomes"
+
+*Source: 2021 Value of IT Certification Report, Pearson VUE
+
+Earning Power
+Holding a certification helps you get a higher salary, benefiting you in the long term.
+
+28%
+received a salary or wage increase
+55%
+were rewarded within three months
+77%
+were rewarded within six months
+
+==
+*Source: 2021 Value of IT Certification Report, Pearson VUE
+
+Job Satisfaction
+Whether it's because of advanced skills, higher pay, better opportunities, or special accolades, certification helps to increase job satisfaction.
+
+91%
+increased confidence
+76%
+greater job satisfaction
+74%
+greater work autonomy and independence
+
+*Source: 2021 Value of IT Certification Report, Pearson VUE
+
+==
+Increased opportunity and recognition
+The second most important motivation for pursuing certification is "to enhance a professional profile". In the business world, being perceived as competent and capable is directly related to the opportunities you receive.
+
+41%
+got a promotion
+79%
+agree their skills are in greater demand
+70%
+getting certified has increased their ability to solve problems
+
+*Source: 2022 Salary Survey, Certification Magazine
+
